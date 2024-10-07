2b9d16ac39f700617fd6a26b44628304f299ee11
Gudhe Jagadeesh <jagadeesh@example.com>
2024-10-07 17:41:29 +0530

    solution4

--- challange#4udemy.js
+++ challange#4udemy.js
@@ -24,4 +24,6 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${
 
 const bill = 275;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+console.log(`The bill was ${bill},
+             the tip was ${tip}, 
+             and the total value ${bill + tip}`);
