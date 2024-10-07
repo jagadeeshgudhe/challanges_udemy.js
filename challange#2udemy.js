822db023ad11a0bb3a2ed27521236cbf6ef30d1f
Gudhe Jagadeesh <jagadeesh@example.com>
2024-10-07 17:38:45 +0530

    solution2

--- challange#2udemy.js
+++ challange#2udemy.js
@@ -19,4 +19,4 @@ if (BMIMark>BMIJohn){
     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 }else{
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
-}
+};
